31082bb93b53543c3a0bf1d2c5acd2787dcd890e
Yoan Thirion <yoan.thirion@gmail.com>
2022-11-16 17:50:42 +0100

    add NIR validation TDD

---
+++ solution/f#/Nir-Kata-Tests/NIRTests.fs
@@ -0,0 +1,30 @@
+module NIRTests
+
+open Nir_Kata.Primitive.Obsession
+open Xunit
+
+[<Theory>]
+[<InlineData("2230", "too short")>]
+[<InlineData("323115935012322", "incorrect sex")>]
+[<InlineData("2ab115935012322", "incorrect year")>]
+[<InlineData("223ab5935012322", "incorrect month")>]
+[<InlineData("223145935012322", "incorrect month 2")>]
+[<InlineData("223005935012322", "incorrect month 3")>]
+[<InlineData("22311xx35012322", "incorrect department")>]
+[<InlineData("223119635012322", "incorrect department 2")>]
+[<InlineData("2231159zzz12322", "incorrect city")>]
+[<InlineData("2231159123zzz22", "incorrect serial number")>]
+[<InlineData("223115935012321", "incorrect control key")>]
+let ``Validate Nir should return false`` (input: string, reason: string) =
+    Assert.False(NIR.validate input, reason)
+
+[<Theory>]
+[<InlineData("223115935012322")>]
+[<InlineData("200029923123486")>]
+[<InlineData("254031088723464")>]
+[<InlineData("195017262676215")>]
+[<InlineData("155053933981739")>]
+[<InlineData("106099955391094")>]
+[<InlineData("221035671987783")>]
+[<InlineData("171108279305362")>]
+let ``Validate Nir should return true`` (input: string) = Assert.True(NIR.validate input)
---
+++ solution/f#/Nir-Kata/Primitive.Obsession/NIR.fs
@@ -0,0 +1,54 @@
+﻿namespace Nir_Kata.Primitive.Obsession
+
+open System
+
+module NIR =
+    let private validNIRLength = 15
+    let private isANumber str = str |> Seq.forall Char.IsDigit
+
+    let private readNumber str : Option<int64> =
+        if isANumber str then
+            Some(str |> int64)
+        else
+            None
+
+    let private validateLength (input: string, continueWith: string -> bool) =
+        if input.Length = validNIRLength then
+            continueWith input
+        else
+            false
+
+    let private validateSex sex = sex = '1' || sex = '2'
+    let private validateYear year = isANumber year
+
+    let private validateNumber (str: string, rule: int64 -> bool) =
+        match readNumber str with
+        | Some n -> rule n
+        | _ -> false
+
+    let private validateMonth month =
+        validateNumber (month, (fun x -> x > 0 && x <= 12))
+
+    let private validateDepartment department =
+        validateNumber (department, (fun m -> (m > 0 && m <= 95) || m = 99))
+
+    let private validateCity city = isANumber city
+    let private validateSerialNumber serialNumber = isANumber serialNumber
+    let private calculateKey number : int64 = 97L - (number % 97L)
+
+    let private validateKey number key =
+        match (readNumber number, readNumber key) with
+        | Some n, Some k -> calculateKey n = k
+        | _ -> false
+
+    let private validateCharacters (input: string) =
+        validateSex input.[0]
+        && validateYear input.[1..2]
+        && validateMonth input.[3..4]
+        && validateDepartment input.[5..6]
+        && validateCity input.[7..9]
+        && validateSerialNumber input.[10..12]
+        && validateKey input.[0..12] input.[13..14]
+
+    let validate input =
+        validateLength (input, validateCharacters)
