c592af31f14234f648d02271693d4392f1283705
Yoan Thirion <yoan.thirion@gmail.com>
2023-03-23 16:58:57 +0400

    remove duplication

--- solution/java/src/main/java/primitive/obsession/StringExtensions.java
+++ solution/java/src/main/java/primitive/obsession/StringExtensions.java
@@ -1,5 +1,6 @@
 package primitive.obsession;
 
+import io.vavr.Function1;
 import io.vavr.control.Option;
 import lombok.experimental.UtilityClass;
 
@@ -9,18 +10,20 @@ import static io.vavr.control.Option.some;
 @UtilityClass
 public class StringExtensions {
     public static Option<Integer> toInt(String potentialNumber) {
-        return isANumber(potentialNumber)
-                ? some(Integer.parseInt(potentialNumber))
-                : none();
+        return toNumber(potentialNumber, Integer::parseInt);
     }
 
     public static Option<Long> toLong(String potentialNumber) {
-        return isANumber(potentialNumber)
-                ? some(Long.parseLong(potentialNumber))
-                : none();
+        return toNumber(potentialNumber, Long::parseLong);
     }
 
     public static boolean isANumber(String str) {
         return str != null && str.matches("[0-9.]+");
+    }
+
+    private static <T> Option<T> toNumber(String input, Function1<String, T> parser) {
+        return isANumber(input)
+                ? some(parser.apply(input))
+                : none();
     }
 }
